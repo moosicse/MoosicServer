b3bf6947cbaa88a31dde514ac889b84da3bda8cb
Touko <HoshinoTouko@users.noreply.github.com>
2019-05-20 17:09:08 +0800

    Link admin page (#4)
    * Link admin page.

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from account.models import UserGroup, UserProfile
+
+
+@admin.register(UserProfile, UserGroup)
+class AccountAdmin(admin.ModelAdmin):
+    pass
--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from music.models import Genre, Singer, Album, Song, Playlist
+
+
+@admin.register(Genre, Singer, Album, Song, Playlist)
+class AccountAdmin(admin.ModelAdmin):
+    pass
