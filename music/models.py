cfe20397ec264384c2a857a71c161a385a8a7a87
HoshinoTouko <oranly@hotmail.com>
2019-05-24 00:09:13 +0800

    Modify models.

--- music/models.py
+++ music/models.py
@@ -7,6 +7,9 @@ from music.constants import SEX_CHOICES
 class Genre(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 
 class Singer(models.Model):
     name = models.CharField(max_length=20)
@@ -15,6 +18,9 @@ class Singer(models.Model):
     country = models.CharField(max_length=20, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Album(models.Model):
     name = models.CharField(max_length=20, blank=True, null=True)
@@ -22,19 +28,28 @@ class Album(models.Model):
     published_date = models.DateField(null=True, blank=True)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Song(models.Model):
     name = models.CharField(max_length=20)
     singer = models.ForeignKey(Singer, blank=True, null=True, on_delete=models.SET_NULL)
     album = models.ForeignKey(Album, blank=True, null=True, on_delete=models.SET_NULL)
-    genres = models.ManyToManyField(Genre, related_name='song_set')
+    genres = models.ManyToManyField(Genre, blank=True, null=True, related_name='song_set')
     mood = models.TextField(default='{}')
-    user_group = models.ManyToManyField('account.UserGroup', related_name='song_set')
-    file = models.FileField(blank=True, null=True)
+    user_group = models.ManyToManyField('account.UserGroup', blank=True, null=True, related_name='song_set')
+    file = models.FileField(upload_to='music', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Playlist(models.Model):
     name = models.CharField(max_length=20)
     songs = models.ManyToManyField(Song, related_name='playlist_set')
     created_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
