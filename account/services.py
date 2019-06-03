6f01f0bbf2d059df69218c41b0946a302c563d56
Touko <HoshinoTouko@users.noreply.github.com>
2019-06-03 18:14:17 +0800

    Init register, login and logout api. (#13)

--- account/services.py
+++ account/services.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 from account.models import UserProfile
 from music.models import Song
@@ -16,3 +17,11 @@ class UserServices:
             if user_profile in user_group.users.all():
                 return True
         return False
+
+
+class AccountServices:
+    @classmethod
+    def get_user_by_email_or_name(cls, email_or_name):
+        return UserProfile.objects.filter(
+            Q(user__username=email_or_name) | Q(user__email=email_or_name)
+        ).first()
